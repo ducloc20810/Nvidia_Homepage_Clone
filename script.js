5dfc4cdf7c8b7310a557f979bbdd181e9079e06d
Duc Loc <ducloc0009@gmail.com>
2022-04-13 19:23:00 +0700

    update bug

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const sliderDiv = document.querySelector(".slider");
 const sliders = document.querySelectorAll(".slider__inner");
 let activeSlide = document.querySelector(".slider__inner--active");
 
@@ -5,42 +6,113 @@ const pages = document.querySelectorAll(".slider__paginate li");
 
 let activePage = document.querySelector(".active-page");
 
+//Slide handle
 function changeSlide(index) {
-  activeSlide.classList.remove("slider__inner--active");
-  activeSlide = sliders[index];
-  sliders[index].classList.add("slider__inner--active");
+    clearInterval(autoTime);
+    activeSlide.classList.remove("slider__inner--active");
+    activeSlide = sliders[index];
+    sliders[index].classList.add("slider__inner--active");
 
-  activePage.classList.remove("active-page");
-  activePage = pages[index];
-  pages[index].classList.add("active-page");
+    activePage.classList.remove("active-page");
+    activePage = pages[index];
+    pages[index].classList.add("active-page");
+    autoTime = setInterval(() => {
+        autoChangeSlide();
+    }, 6000);
 }
 
 Array.from(pages).forEach(
-  (page, index) =>
-    (page.onclick = function () {
-      clearInterval(autoTime);
-      changeSlide(index);
-      autoTime = setInterval(() => {
-        autoChangeSlide();
-      }, 6000);
-    })
+    (page, index) =>
+        (page.onclick = function () {
+
+            changeSlide(index);
+
+        })
 );
 
-function autoChangeSlide() {
-  let activeIndex = Array.from(pages).findIndex((page) => page === activePage);
-  if (activeIndex === 4) {
-    changeSlide(0);
-  } else {
-    changeSlide(activeIndex + 1);
-  }
+function autoChangeSlide(opt = "forward") {
+    let activeIndex = Array.from(pages).findIndex((page) => page === activePage);
+
+    if (opt === "forward") {
+        if (activeIndex === 4) {
+            changeSlide(0);
+        } else {
+            changeSlide(activeIndex + 1);
+        }
+    } else if (opt === "backward") {
+        if (activeIndex === 0) {
+            changeSlide(4);
+        } else {
+            changeSlide(activeIndex - 1);
+        }
+    }
 }
 
 let autoTime = setInterval(() => {
-  autoChangeSlide();
+    autoChangeSlide();
 }, 6000);
+
+
+//Handle swipe
+sliderDiv.addEventListener('touchstart', handleTouchStart, false);
+sliderDiv.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+    return evt.touches;          // browser API
+
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    let xUp = evt.touches[0].clientX;
+    let yUp = evt.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff))
+        //Horizontal swipe
+    {/*most significant*/
+        if (xDiff > 0) {
+            /* right to left swipe */
+            autoChangeSlide();
+
+        } else {
+            /* left to right swipe */
+            autoChangeSlide("backward");
+
+
+        }
+    } else
+        //Verical swipe
+    {
+        if (yDiff > 0) {
+            /* down swipe */
+        } else {
+            /* up swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+}
+
 
 // Handle list scroll
 const list = document.querySelectorAll(".section__list-container ul");
+const item = document.querySelector(".card");
 const btnLefts = document.querySelectorAll(".btn-left");
 const btnRights = document.querySelectorAll(".btn-right");
 
@@ -49,56 +121,75 @@ console.log(btnLefts);
 console.log(btnRights);
 
 Array.from(btnLefts).forEach((btn, index) => {
-  btn.onclick = function () {
-    scrollToRight(index);
-  };
+    btn.onclick = function () {
+        scrollToRight(index);
+    };
 });
 
 Array.from(btnRights).forEach((btn, index) => {
-  btn.onclick = function () {
-    scrollToLeft(index);
-  };
+    btn.onclick = function () {
+        scrollToLeft(index);
+    };
 });
 
 function scrollToLeft(index) {
-  const leftOffSet = list[index].scrollLeft;
+    const leftOffSet = list[index].scrollLeft;
 
-  const maxSrollWidth = list[index].scrollWidth;
+    const maxScrollWidth = list[index].scrollWidth;
 
-  const listWidth = list[index].clientWidth;
-  const newLeftOffSet = leftOffSet + 295;
+    const listWidth = list[index].clientWidth;
 
-  if (leftOffSet < maxSrollWidth - listWidth) {
-    btnLefts[index].classList.remove("hide");
-  }
+    const items = list[index].querySelectorAll(".card");
+    let activeItem = list[index].querySelector(".card.active");
+    const activeIndex = Array.from(items).findIndex(item => item === activeItem);
+    activeItem.classList.remove("active");
+    activeItem = items[activeIndex + 1];
 
-  list[index].scroll({
-    left: newLeftOffSet,
-    behavior: "smooth",
-  });
+    activeItem.classList.add("active");
 
-  if (newLeftOffSet >= maxSrollWidth - listWidth) {
-    btnRights[index].classList.add("hide");
-  }
+
+    const newLeftOffSet = activeItem.offsetLeft;
+
+
+    if (leftOffSet < maxScrollWidth - listWidth) {
+        btnLefts[index].classList.remove("hide");
+    }
+
+    list[index].scroll({
+        left: newLeftOffSet,
+        behavior: "smooth",
+    });
+
+    if (newLeftOffSet >= maxScrollWidth - listWidth) {
+        btnRights[index].classList.add("hide");
+    }
 }
 
 function scrollToRight(index) {
-  const leftOffSet = list[index].scrollLeft;
+    const leftOffSet = list[index].scrollLeft;
 
-  const newLeftOffSet = leftOffSet - 295;
+    const items = list[index].querySelectorAll(".card");
+    let activeItem = list[index].querySelector(".card.active");
+    const activeIndex = Array.from(items).findIndex(item => item === activeItem);
+    activeItem.classList.remove("active");
+    activeItem = items[activeIndex - 1];
+    activeItem.classList.add("active");
 
-  if (leftOffSet > 0) {
-    btnRights[index].classList.remove("hide");
-  }
 
-  list[index].scroll({
-    left: newLeftOffSet,
-    behavior: "smooth",
-  });
+    const newLeftOffSet = activeIndex === 1 ? 0 : activeItem.offsetLeft;
 
-  if (newLeftOffSet <= 0) {
-    btnLefts[index].classList.add("hide");
-  }
+    if (leftOffSet > 0) {
+        btnRights[index].classList.remove("hide");
+    }
+
+    list[index].scroll({
+        left: newLeftOffSet,
+        behavior: "smooth",
+    });
+
+    if (newLeftOffSet <= 0) {
+        btnLefts[index].classList.add("hide");
+    }
 }
 
 // Handle mobile header
@@ -111,36 +202,36 @@ const closeSearchBtns = document.querySelectorAll(".close-search-btn");
 const searchLinks = document.querySelectorAll(".search");
 
 Array.from(searchBtns).forEach(
-  (btn, index) =>
-    (btn.onclick = function openSearch() {
-      closeNavSide();
-      searchLinks[index].classList.add("search--open");
-      searchDivs[index].classList.remove("hide");
-      closeSearchBtns[index].classList.remove("hide");
-      btn.classList.add("hide");
-      layout.classList.remove("hide");
-    })
+    (btn, index) =>
+        (btn.onclick = function openSearch() {
+            closeNavSide();
+            searchLinks[index].classList.add("search--open");
+            searchDivs[index].classList.remove("hide");
+            closeSearchBtns[index].classList.remove("hide");
+            btn.classList.add("hide");
+            layout.classList.remove("hide");
+        })
 );
 
 function closeSearch(index) {
-  searchLinks[index].classList.remove("search--open");
-  searchDivs[index].classList.add("hide");
-  closeSearchBtns[index].classList.add("hide");
-  searchBtns[index].classList.remove("hide");
-  layout.classList.add("hide");
+    searchLinks[index].classList.remove("search--open");
+    searchDivs[index].classList.add("hide");
+    closeSearchBtns[index].classList.add("hide");
+    searchBtns[index].classList.remove("hide");
+    layout.classList.add("hide");
 }
 
 Array.from(closeSearchBtns).forEach((btn, index) => {
-  btn.onclick = function () {
-    closeSearch(index);
-  };
+    btn.onclick = function () {
+        closeSearch(index);
+    };
 });
 
 layout.addEventListener("click", function () {
-  closeSearch(0);
-  closeSearch(1);
+    closeSearch(0);
+    closeSearch(1);
 
-  closeNavSide();
+    closeNavSide();
 });
 
 // Nav
@@ -149,19 +240,19 @@ const navSide = document.querySelector(".navbar__side");
 const closeNavBtn = document.querySelector(".close-navbar-side");
 
 navBtn.addEventListener("click", function openNav() {
-  closeSearch(0);
-  closeSearch(1);
-  closeNavBtn.classList.remove("hide");
-  layout.classList.remove("hide");
-  navSide.classList.add("navbar__side--open");
-  navBtn.classList.add("hide");
+    closeSearch(0);
+    closeSearch(1);
+    closeNavBtn.classList.remove("hide");
+    layout.classList.remove("hide");
+    navSide.classList.add("navbar__side--open");
+    navBtn.classList.add("hide");
 });
 
 function closeNavSide() {
-  closeNavBtn.classList.add("hide");
-  layout.classList.add("hide");
-  navSide.classList.remove("navbar__side--open");
-  navBtn.classList.remove("hide");
+    closeNavBtn.classList.add("hide");
+    layout.classList.add("hide");
+    navSide.classList.remove("navbar__side--open");
+    navBtn.classList.remove("hide");
 }
 
 closeNavBtn.addEventListener("click", closeNavSide);
